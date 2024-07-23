21216d4bb9c0696ef8f307f397cd570d2018b6f4
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-23 22:01:52 +0530

    Setup project & Make navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/global/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
+      
+
+    </main>
+  );
+}
